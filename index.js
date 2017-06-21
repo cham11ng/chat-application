9036acad9f31ee7fb7db9f7cca2be38366a71d32
Sagar Chamling <sgr.raee@gmail.com>
2017-06-21 23:11:51 +0545

    Add socket.io and make connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 server.listen(3000, function() {
 	console.log('Running server 127.0.0.1:3000');
@@ -11,4 +12,8 @@ app.get('/', function(request, response) {
 
 app.get('/home', function(request, response) {
 	response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('A connection has been made.');
 });
